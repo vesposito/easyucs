255bfb8a8530a83a0f4addfd0a31aefcc1594750
Vincent Esposito <vesposito@users.noreply.github.com>
2022-08-24 14:19:16 +0200

    Bump version to 0.9.7.6

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 __author__ = "Marc Abu El Ghait, Franck Bonneau and Vincent Esposito"
 __copyright__ = "Copyright 2016-2022, Cisco"
 
-__version__ = "0.9.7.5"
+__version__ = "0.9.7.6"
 __status__ = "Development"
 
 # https://realpython.com/pypi-publish-python-package/
